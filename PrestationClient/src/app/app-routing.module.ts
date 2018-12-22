bbba52105fbc49b54bf1e085a991ca1ee0f23cb1
Cyril Paulus <paulus.cyril@gmail.com>
2018-12-22 12:27:38 +0100

    Add partial routes for prestations

--- PrestationClient/src/app/app-routing.module.ts
+++ PrestationClient/src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   { path: 'prestations', component: PrestationComponent },
+  { path: 'prestations/:user', component: PrestationComponent },
+  { path: 'prestations/:user/:year', component: PrestationComponent },
   { path: 'prestations/:user/:year/:month', component: PrestationComponent },
   { path: 'codes-chantier', component: CodeChantierComponent },
   { path: 'users', component: UserComponent }
